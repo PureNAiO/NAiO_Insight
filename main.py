72f7d3dfcd478eea72d5b8b46cb87cde879c0fb1
Jackal.C <ccxxll23@163.com>
2024-03-14 23:29:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,6 @@ from pydantic import BaseModel
 from fastapi import FastAPI
 from utils import Influx
 
-import uvicorn
-
 app = FastAPI()
 influx = Influx()
 
@@ -19,6 +17,3 @@ async def zabbix_recv(data: ZabbixData):
     metrics = data['datas']
     for metric in metrics.items():
         influx.write(pname='device_name', field_tup=metric)
-
-if __name__ == "__main__":
-    uvicorn.run(app, host="0.0.0.0", port=5002, log_level="error")
